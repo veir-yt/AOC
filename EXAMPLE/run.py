736f7853aa9d41a36547f16226423490f3b3fab5
VEIR <veir.business@gmail.com>
2024-11-30 22:56:22 -0600

    update example

--- EXAMPLE/run.py
+++ EXAMPLE/run.py
@@ -26,7 +26,21 @@ def part1():
     return ans 
 
 def part2():
-    pass
+    data = getFile(os.path.join(os.curdir, 'input.txt')).strip()
+
+    data = data.split('\n')
+    print(data)
+
+    ans = 0
+
+    arr = []
+
+    for index in range(len(data)):
+        line = data[index]
+        # arr.append(int(line))
+        print(line)
+        
+    return ans 
 
 print(f'part1: {part1()}')
 print(f'part2: {part2()}')
